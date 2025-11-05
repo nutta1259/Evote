14159a0c4e9546b1a7cc798af9fa133a14ca33c6
nutta1259 <nutta1259@gmail.com>
2025-11-05 09:26:49 +0700

    25681105

---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/scripts',
+      (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+    vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  
+})
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    homepage
+    <v-btn color="primary">vuetify Button</v-btn>
+  <slot />
+  </div>
+</template>
